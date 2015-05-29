8c02ebe09fed93d1b23d0ed35881fea046f6ee77
vitaly.grinko <vitaly.grinko@nytimes.com>
2015-05-28 23:02:11 -0400

    added support for multiple urls, switched phantomjs integration module, added screenshot generation feature

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,80 +8,87 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'lib/*.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
-    },
-
-    // Configuration to be run (and then tested).
-    missingassetchecker: {
-      options: {
-        issues : ["javascripterror","networkerror"]
-      },
-      home: {
-        options: {
-          url: 'http://localhost:9000/',
-          
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'lib/*.js',
+                'tasks/*.js',
+                '<%= nodeunit.tests %>',
+            ],
+            options: {
+                jshintrc: '.jshintrc',
+            },
         },
-      },
-      javascripterror: {
-        options: {
-          url: 'http://localhost:9000/javascripterror.html',
-          issues : ["javascripterror"]
-          
-        },
-      },
-    },
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
-
-    express: {
-        options: {
-            port: 9999,
-            background: true
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: ['tmp'],
         },
-        test : {
-          options: {
-            script: 'test/server.js'
+
+        // Configuration to be run (and then tested).
+        missingassetchecker: {
+            options: {
+                issues: ["javascripterror", "networkerror"]
+            },
+            //test: {
+            //    "options": {
+            //        url: 'http://www.google.com',
+            //        "report": 'tmp/test',
+            //        "screenshots": 'tmp/test/screenshots/',
+            //    },
+            //},
+            home: {
+                options: {
+                    url: 'http://localhost:9000/',
+
+                },
+            },
+            javascripterror: {
+                options: {
+                    url: 'http://localhost:9000/javascripterror.html',
+                    issues: ["javascripterror"]
+
+                },
+            },
+        },
+
+        // Unit tests.
+        nodeunit: {
+            tests: ['test/*_test.js'],
+        },
+
+        express: {
+            options: {
+                port: 9999,
+                background: true
+            },
+            test: {
+                options: {
+                    script: 'test/server.js'
+                }
             }
         }
-    }
 
-  });
+    });
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-express-server');
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-express-server');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'express:test', 'nodeunit']);
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('test', ['clean', 'express:test', 'nodeunit']);
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'test']);
 
 };
